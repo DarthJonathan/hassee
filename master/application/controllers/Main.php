b652c48739cda1191e03daf05bfb8cd6818187df
Jonathan Hosea <jonathan.hosea@me.com>
2016-07-25 10:26:47 +0700

    Added CI Files

---
+++ master/application/controllers/Main.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	public function index()
+	{
+		if(isset($this->session->isLogged === True))
+		{
+			$this->load->view('dashboard.php');
+		}else
+		{
+			redirect('/main/login');
+		}
+	}
+}
